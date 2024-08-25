b15501276ebf51b6f77bf3082d3ac38f7be591f4
SaeedAmini <saeedamini506750@gmail.com>
2024-08-25 08:22:53 -1200

    create page show product

--- aminicar/urls.py
+++ aminicar/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from aminicar.views import index
+from aminicar.views import index,show_product
+
+
 
 urlpatterns = [
-    path('index', index , name='index')
+    path('show_product', show_product , name='show_product'),
+      path('index', index , name='index')
 ]
 
--- aminicar/views.py
+++ aminicar/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def index (requset):
     return render(requset,'aminicar/main/index.html')
+
+def show_product (requset):
+    return render(requset,'aminicar/main/show_product.html')
