38bd5b7b28197de0de9bb5571341ba7dbb63119f
SaeedAmini <saeedamini506750@gmail.com>
2024-09-23 20:43:48 +0330

    create cart js page add write some code for add_to_cart in show_product page

--- product/views.py
+++ product/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Product ,Cart
 from django.http import JsonResponse
 import json
-
+ 
 def view_product(request):
     context = {}
     products = Product.objects.all()
@@ -10,6 +10,7 @@ def view_product(request):
     context['products'] = products
 
     return render(request, 'aminicar/main/index.html', context)
+
 
 
 
@@ -21,3 +22,15 @@ def product_detail(request, product_id):
 
     return render(request, 'aminicar/main/show_product.html', context)
 
+
+
+def add_to_cart(request):
+    if request.method == "POST":
+        if request.user.is_authenticated:
+            product_id = int(request.POST.get('product_id'))
+            print('hello saeedamini   product_id', product_id)
+            # منطق اضافه کردن محصول به سبد خرید
+            return JsonResponse({'status': "Product added successfully!"})
+        else:
+            return JsonResponse({'status': "login to continue"})
+    return JsonResponse({'status': "Invalid request"}, status=400)
---
+++ static/js/cart.js
@@ -0,0 +1,30 @@
+$('.add-to-cart-btn').click(function (e) {
+    e.preventDefault();
+    console.log("Button Clicked");
+
+    let product_id = $(this).closest('.product-details').find('.prodt_id').val();
+    console.log("Product ID:", product_id);
+
+    let token = $('input[name=csrfmiddlewaretoken]').val();
+    console.log("CSRF Token:", token);
+
+    $.ajax({
+        method: "POST",
+        url: "/add_to_cart/",  // مسیر به‌روز شده
+        data: {
+            'product_id': product_id,
+            'csrfmiddlewaretoken': token
+        },
+        success: function (response) {
+            console.log("Response:", response);
+            if (response.status === "Product added successfully!") {
+                alertify.success(response.status);
+            } else {
+                alertify.error(response.status);
+            }
+        },
+        error: function (xhr, status, error) {
+            console.log("Error:", error);
+        }
+    });
+});
