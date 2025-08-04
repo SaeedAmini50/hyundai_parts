a5aebb811ac657206531f47bd37f57c237f48e50
saeed amini <saeedamini506750@gmail.com>
2025-08-05 00:48:12 +0100

    setting for render host

--- smartcar/settings.py
+++ smartcar/settings.py
@@ -44,6 +44,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMeddLeware'
 ]
 
 ROOT_URLCONF = 'smartcar.urls'
